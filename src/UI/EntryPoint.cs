a5b26ed7b6074c55e8e890b0cd7557c028634262
pepelev <pepelev@skbkontur.ru>
2023-01-18 01:24:07 +0500

    Adds solution

---
+++ src/UI/EntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Avalonia;
+
+namespace Zenki.UI;
+
+internal static class EntryPoint
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        var app = BuildAvaloniaApp();
+        app.StartWithClassicDesktopLifetime(args);
+    }
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace();
+}
