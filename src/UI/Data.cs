a459508f4215daca86590a473aa93a09f73ca0e1
pepelev <pepelev@skbkontur.ru>
2023-01-23 22:41:39 +0500

    Implement stupid search

---
+++ src/UI/Data.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Zenki.UI;
+
+public sealed class Data : INotifyPropertyChanged
+{
+    private string filePath = @"C:\Users";
+    private string query = "";
+    private TrigramIndex<char, (int Index, string Line)>? index;
+
+    public string FilePath
+    {
+        get => filePath;
+        set
+        {
+            if (value == filePath)
+            {
+                return;
+            }
+
+            if (!File.Exists(value))
+            {
+                filePath = value;
+                return;
+            }
+
+            var lines = File.ReadAllLines(value, Encoding.UTF8);
+            var newIndex = new TrigramIndex<char, (int Index, string Line)>();
+            for (var i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                var trigrams = Trigram.StringToTrigrams(line);
+                foreach (var trigram in trigrams)
+                {
+                    newIndex.Add(trigram, (i, line));
+                }
+            }
+
+            (index, filePath) = (newIndex, value);
+        }
+    }
+
+    public string Query
+    {
+        get => query;
+        set
+        {
+            if (value == query)
+            {
+                return;
+            }
+
+            if (index == null)
+            {
+                query = value;
+                return;
+            }
+
+            var list = Search(value, index).Take(20);
+            Found = string.Join("\n", list);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Found)));
+            query = value;
+        }
+    }
+
+    private static IEnumerable<string> Search(string query, ITrigramIndex<char, (int Index, string Line)> index)
+    {
+        var head = Trigram.StringToTrigrams(query).Take(1).ToArray();
+        if (head.Length == 0)
+        {
+            return Enumerable.Empty<string>();
+        }
+
+        return index.Search(head[0]).Select(entry => entry.Line);
+    }
+
+    public string? Found { get; set; }
+    public event PropertyChangedEventHandler? PropertyChanged;
+}
