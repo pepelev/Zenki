3029ec694c85adf760e910441f116eb3dce14929
Виталий Семёнов <wizzem1110@hotmail.com>
2023-05-11 13:09:01 +0500

    tests & constructors

--- src/UI/Filter.cs
+++ src/UI/Filter.cs
@@ -12,6 +12,12 @@ public sealed class FieldFilter : Filter
 {
     private readonly string field;
     private readonly string value;
+
+    public FieldFilter(string field, string value)
+    {
+        this.field = field;
+        this.value = value;
+    }
     
     public override bool Pass(LogEntry entry)
     {
@@ -24,6 +30,12 @@ public sealed class AndFilter : Filter
     private readonly Filter a;
     private readonly Filter b;
 
+    public AndFilter(Filter b, Filter a)
+    {
+        this.b = b;
+        this.a = a;
+    }
+
     public override bool Pass(LogEntry entry) => a.Pass(entry) & b.Pass(entry);
 
 }
@@ -32,6 +44,12 @@ public sealed class OrFilter : Filter
 {
     private readonly Filter a;
     private readonly Filter b;
+
+    public OrFilter(Filter a, Filter b)
+    {
+        this.a = a;
+        this.b = b;
+    }
 
     public override bool Pass(LogEntry entry) => a.Pass(entry) | b.Pass(entry);
 }
